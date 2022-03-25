8a21dced3b8411dfe47b057e9450b7f663ce3fea
Hamid <www.abdulhamid3311@gmail.com>
2022-03-26 00:02:35 +0600

    added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Watch Shop</h2>
+    <div >
+      <Header></Header>
 
     </div>
   );
